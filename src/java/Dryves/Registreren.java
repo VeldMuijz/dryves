a2d026a10dc6e0d9a10810edd72cf73f331d1d9c
heuvenk <kvheuv@gmail.com>
2013-04-17 19:47:29 +0200

    Registreren de setParameters en het schrijven naar de console.

--- src/java/Dryves/Registreren.java
+++ src/java/Dryves/Registreren.java
@@ -100,11 +100,68 @@ public class Registreren extends HttpServlet {
         
         //Zet de achternaam
         lid.setAnaam(request.getParameter("anaam"));
-        
         //Print achternaam naar console
         System.out.println("Dit is de achternaam: " + lid.getAnaam());
         
+         //Zet tvoegsel
+        lid.setTvoegsel(request.getParameter("tvoegsel"));
+        //Print tvoegsel naar de console
+        System.out.println("Dit is de tussenvoegsel: " + lid.getTvoegsel());
         
+        //Zet het geslacht
+        lid.setGeslacht(request.getParameter("geslacht"));
+        //Print geslacht naar de console
+        System.out.println("Dit is het geslacht: " + lid.getGeslacht());
+        
+        //Zet de straat
+        lid.setStraat(request.getParameter("straat:"));
+        //Print de straat naar de console
+        System.out.println("Dit is de straat: " + lid.getStraat());
+        
+        //Zet het huisnummer
+        lid.setHuisnummer(request.getParameter("huisnummer"));
+        //Print het huisnummer naar de console
+        System.out.println("Dit is het huisnummer: " + lid.getHuisnummer());
+        
+        //Zet de postcode
+        lid.setPostcode(request.getParameter("postcode"));
+        //Print de postcode naar de console
+        System.out.println("Dit is de postcode: " + lid.getPostcode());
+        
+        //Zet stad
+        lid.setStad(request.getParameter("stad"));
+        //Print stad naar de console
+        System.out.println("Dit is de stad: " + lid.getStad());
+        
+        //Zet het telefoonummer
+        lid.setTelnr(request.getParameter("telnr"));
+        //Schrijf telefoonnummer naar de console
+        System.out.println("Dit is het telefoonnummer: " + lid.getTelnr());
+        
+        //Zet het rekeningnummer 
+        lid.setReknr(request.getParameter("reknr"));
+        //Schrijf het rekeningnummer naar de console
+        System.out.println("Dit is het rekeningnummer: " + lid.getReknr());
+        
+        //Zet het email adres
+        lid.setEmail(request.getParameter("email"));
+        //Schrijf het email adres naar de console
+        System.out.println("Dit is het email adres: " + lid.getEmail());
+        
+        //Zet het wachtwoord
+        lid.setWachtwoord(request.getParameter("wachtwoord"));
+        //Schrijf het wachtwoord naar de console
+        System.out.println("Dit is het wachtwoord: " + lid.getWachtwoord());
+        
+        //Zet de fotourl
+        lid.setFotoUrl(request.getParameter("fotoUrl"));
+        //Schrijf de fotourl naar de console
+        System.out.println("Dit is de fotourl: " + lid.getFotoUrl());
+        
+        //Zet de langnotify
+        lid.setLangnotify(request.getParameter("langnotify"));
+        //Schrijf langnotify naar de console
+        System.out.println("Dit is de langnotify: " + lid.getLangnotify());
     }
 
     private static class request {
