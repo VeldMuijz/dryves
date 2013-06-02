6ed99c6a5ba5d04a760aea8e640c35afdebc5618
VeldMuijz <veldmuijz@gmail.com>
2013-06-02 14:22:17 +0200

    testmaster

--- src/java/Dryves/Controller/Login.java
+++ src/java/Dryves/Controller/Login.java
@@ -20,70 +20,70 @@ import org.hibernate.Session;
  */
 public class Login extends HttpServlet {
 
-    Session session = null;
+	Session session = null;
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, java.io.IOException {
-    }
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+	}
 
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	/**
+	 * Handles the HTTP
+	 * <code>POST</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        try {
+		try {
 
-            Lid user = new Lid();
+			Lid user = new Lid();
 
-            user.setEmail(request.getParameter("email"));
-            user.setWachtwoord(request.getParameter("wachtwoord"));
+			user.setEmail(request.getParameter("email"));
+			user.setWachtwoord(request.getParameter("wachtwoord"));
 
-            user = LidDao.login(user);
+			user = LidDao.login(user);
 
-            if (user.isValid()) {
+			if (user.isValid()) {
 
-                //Hieronder wordt bepaald of het lid admin is of niet.
-                                    
-                HttpSession session = request.getSession(true);
-                session.setAttribute("currentSessionUser", user);
-                
-                LidDao dao = new LidDao();
-		dao.adminLogin(user);
+				//Hieronder wordt bepaald of het lid admin is of niet.
 
-                if (user.getRol() == 1) {
+				HttpSession session = request.getSession(true);
+				session.setAttribute("currentSessionUser", user);
 
-                    request.getRequestDispatcher("WEB-INF/mijndryves.jsp").forward(request, response);
+				LidDao dao = new LidDao();
+				dao.adminLogin(user);
 
-                } else if (user.getRol() == 2) {
-                    request.getRequestDispatcher("WEB-INF/admin.jsp").forward(request, response);
+				if (user.getRol() == 1) {
 
-                }
-            } else {
+					request.getRequestDispatcher("WEB-INF/mijndryves.jsp").forward(request, response);
 
-                System.out.println("Het inloggen is niet gelukt, probeer het opnieuw!");
-                response.sendRedirect("login.jsp"); //Retry login 
-            }
-        } catch (Throwable theException) {
-            System.out.println(theException);
-        }
-    }
+				} else if (user.getRol() == 2) {
+					request.getRequestDispatcher("WEB-INF/admin.jsp").forward(request, response);
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+				}
+			} else {
+
+				System.out.println("Het inloggen is niet gelukt, probeer het opnieuw!");
+				response.sendRedirect("login.jsp"); //Retry login 
+			}
+		} catch (Throwable theException) {
+			System.out.println(theException);
+		}
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 }
