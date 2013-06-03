89bf7e1a0d7f3a7e7d881a21491b857596feba55
VeldMuijz <veldmuijz@gmail.com>
2013-06-03 20:37:11 +0200

    Javadoc toegevoegd

--- src/java/Dryves/DatumConverter.java
+++ src/java/Dryves/DatumConverter.java
@@ -130,16 +130,17 @@ public class DatumConverter {
 	}
 
 	/**
-	  Deze methode geeft een datum terug die in deze klasse is geset
+	 * Deze methode geeft een datum terug die in deze klasse is geset
 	 *
 	 * @return datum van het datatype Date
 	 */
 	public Date getDatum() {
 		return datum;
 	}
-	
+
 	/**
 	 * Deze methode set attribuut datum met de parameter datum
+	 *
 	 * @param datum is een datum van het datatype Date
 	 */
 	public void setDatum(Date datum) {
@@ -154,9 +155,10 @@ public class DatumConverter {
 	public Date getEinddatum() {
 		return einddatum;
 	}
-	
+
 	/**
 	 * Deze methode set attribuut einddatum met de parameter einddatum
+	 *
 	 * @param einddatum einddatum van het object type Date
 	 */
 	public void setEinddatum(Date einddatum) {
@@ -171,9 +173,10 @@ public class DatumConverter {
 	public String getStringDatum() {
 		return korteDatum;
 	}
-	
+
 	/**
 	 * Deze methode set attribute Stringdatum met de parameter korteDatum
+	 *
 	 * @param korteDatum is een string wat een datum bevat
 	 */
 	public void setStringDatum(String korteDatum) {
@@ -191,6 +194,7 @@ public class DatumConverter {
 
 	/**
 	 * Deze methode set attribute korteTijd met de parameter korteTijd
+	 *
 	 * @param korteTijd is een string die een korteTijd bevat in formaat HH:mm
 	 */
 	public void setKorteTijd(String korteTijd) {
@@ -205,9 +209,10 @@ public class DatumConverter {
 	public String getKorteDatum() {
 		return korteDatum;
 	}
-	
+
 	/**
 	 * Deze methode set attribute korteDatum met parameter korteDatum
+	 *
 	 * @param korteDatum is een string die een korteDatum bevat dd/mm/yyyy
 	 */
 	public void setKorteDatum(String korteDatum) {
@@ -223,9 +228,10 @@ public class DatumConverter {
 	public DateTime getTimestamp() {
 		return timestamp;
 	}
-	
+
 	/**
 	 * Deze methode set attribute Timestamp met parameter Timestamp
+	 *
 	 * @param timestamp is een DateTime met een timestamp erin
 	 */
 	public void setTimestamp(DateTime timestamp) {
